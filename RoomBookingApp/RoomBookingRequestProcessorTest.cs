1e0deeb3da895bcb20fe2c928852c944e3d362ef
elcanion <elcaniondev@gmail.com>
2024-04-27 00:53:39 -0300

    Just refactoring things.

--- RoomBookingApp/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp/RoomBookingRequestProcessorTest.cs
@@ -1,4 +1,6 @@
-﻿using Shouldly;
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +11,14 @@ namespace RoomBookingApp.Core.Tests
 {
     public class RoomBookingRequestProcessorTest
     {
+        private RoomBookingRequestProcessor _processor;
+
         public object RoomBookingResut { get; private set; }
+        public RoomBookingRequestProcessorTest()
+        {
+            // Arrange
+            _processor = new RoomBookingRequestProcessor();
+        }
 
         [Fact]
         public void Should_Return_Room_Booking_Response_With_Request_Values()
@@ -21,10 +30,9 @@ namespace RoomBookingApp.Core.Tests
                 Email = "test@request.com",
                 Date = new DateTime(2021, 10, 20)
             };
-            var processor = new RoomBookingRequestProcessor();
 
             // Act
-            RoomBookingResult result = processor.BookRoom(request);
+            RoomBookingResult result = _processor.BookRoom(request);
 
             // Assert
             Assert.NotNull(result);
@@ -38,6 +46,14 @@ namespace RoomBookingApp.Core.Tests
             result.FullName.ShouldBe(request.FullName);
             result.Email.ShouldBe(request.Email);
             result.Date.ShouldBe(request.Date);
+        }
+
+        [Fact]
+        public void Should_Throw_Exception_For_Null_Request()
+        {
+            var exception = Should.Throw<ArgumentNullException>(() => _processor.BookRoom(null));
+
+            exception.ParamName.ShouldBe("bookingRequest");
         }
     }
 }
